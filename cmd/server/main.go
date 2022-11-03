1700073dc905669717cb50d6f586d0e5dab5bd1b
Федор Хлебалов <f.khlebalov@qvant.ru>
2022-11-03 20:28:24 +0500

    feat: trying to deploy

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,6 +9,8 @@ import (
 	"gitlab.com/krespix/gamification-api/internal/core/logging"
 	httptransport "gitlab.com/krespix/gamification-api/internal/transport/http"
 	"go.uber.org/zap"
+	"log"
+	"os"
 )
 
 func main() {
@@ -21,6 +23,12 @@ func appStart(ctx context.Context, a *app.App) ([]app.Listener, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+	cfg.HTTP.Port = port
 
 	// Connect to the postgres DB
 	db, err := initDatabase(ctx, cfg, a)
