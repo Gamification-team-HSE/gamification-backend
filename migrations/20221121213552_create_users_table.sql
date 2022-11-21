e6f8c0dca345ae0c63bde779e0c88fff048df771
Федор Хлебалов <f.khlebalov@qvant.ru>
2022-11-21 21:46:16 +0500

    feat: add migration

---
+++ migrations/20221121213552_create_users_table.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+create table users (
+    id bigserial primary key,
+    foreign_id text unique,
+    email text not null unique,
+    created_at timestamp not null,
+    deleted_at timestamp,
+    "role" text
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+drop table users;
+-- +goose StatementEnd
