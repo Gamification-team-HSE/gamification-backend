18502eb57ff963a4c9a7fcb5c3b0be6352d32f91
Федор Хлебалов <f.khlebalov@qvant.ru>
2022-11-22 17:12:39 +0500

    feat: allow methods

--- internal/api/http/http.go
+++ internal/api/http/http.go
@@ -90,6 +90,7 @@ func incrementIncomingRequestsMiddleware(next http.Handler) http.Handler {
 
 func enableCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		next.ServeHTTP(w, r)
 	})
