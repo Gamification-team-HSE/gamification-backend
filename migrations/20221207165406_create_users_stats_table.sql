15fea1737eb6900fce3792d0faf72ce9e715d59b
adselina <ad.selina@mail.ru>
2022-12-07 20:38:36 +0500

    fix migrations

--- migrations/20221207165406_create_users_stats_table.sql
+++ migrations/20221207165406_create_users_stats_table.sql
@@ -1,10 +1,10 @@
 -- +goose Up
 -- +goose StatementBegin
 create table users_stats (
-    user_id bigint,
-    stat_id bigint,
+    user_id bigint references users ON DELETE CASCADE,
+    stat_id bigint references stats ON DELETE CASCADE,
     "value" bigint,
-    updated_at bigint,
+    updated_at timestamp,
     created_at timestamp not null,
     PRIMARY KEY (user_id, stat_id)
 );
