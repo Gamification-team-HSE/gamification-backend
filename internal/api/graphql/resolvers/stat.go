98eb680652ef3d82b459cd3ed0a00e2b95a59b50
adselina <ad.selina@mail.ru>
2022-12-13 23:29:44 +0500

    new function for stat

--- internal/api/graphql/resolvers/stat.go
+++ internal/api/graphql/resolvers/stat.go
@@ -4,7 +4,28 @@ import (
 	"context"
 	"gitlab.com/krespix/gamification-api/internal/models"
 	apiModels "gitlab.com/krespix/gamification-api/pkg/graphql/models"
+	"gitlab.com/krespix/gamification-api/pkg/utils"
 )
+
+func (r *Resolver) GetStat(ctx context.Context, id int) (*apiModels.Stat, error) {
+	stat, err := r.statService.Get(ctx, int64(id))
+	if err != nil {
+		return nil, err
+	}
+	return modelsStatToAPI(stat), nil
+}
+
+func (r *Resolver) GetStats(ctx context.Context) ([]*apiModels.Stat, error) {
+	stats, err := r.statService.List(ctx)
+	if err != nil {
+		return nil, err
+	}
+	res := make([]*apiModels.Stat, 0, len(stats))
+	for _, u := range stats {
+		res = append(res, modelsStatToAPI(u))
+	}
+	return res, nil
+}
 
 func (r *Resolver) CreateStat(ctx context.Context, stat apiModels.NewStat) (interface{}, error) {
 	mStat := &models.Stat{
@@ -14,18 +35,15 @@ func (r *Resolver) CreateStat(ctx context.Context, stat apiModels.NewStat) (inte
 		mStat.Name = stat.Name
 	}
 
-	/* Это тесты?
-	err := r.
+	err := r.statService.Create(ctx, mStat)
 	if err != nil {
 		return nil, err
-	}*/
-
+	}
 	return map[string]interface{}{
 		"status": "success",
 	}, nil
 }
 
-/*
 func modelsStatToAPI(stat *models.Stat) *apiModels.Stat {
 	return &apiModels.Stat{
 		ID:          int(stat.ID),
@@ -37,4 +55,3 @@ func modelsStatToAPI(stat *models.Stat) *apiModels.Stat {
 		SeqPeriod:   utils.SqlNullStringToString(stat.SeqPeriod),
 	}
 }
-*/
