376731f04b53e8a707b7595b222782efd077f429
adselina <ad.selina@mail.ru>
2022-12-14 16:06:19 +0500

    fix stat creation + auth

--- internal/api/graphql/resolvers/stat.go
+++ internal/api/graphql/resolvers/stat.go
@@ -2,19 +2,28 @@ package resolvers
 
 import (
 	"context"
+	"database/sql"
 	"gitlab.com/krespix/gamification-api/internal/models"
 	apiModels "gitlab.com/krespix/gamification-api/pkg/graphql/models"
 )
-
-//error must be here
 
 func (r *Resolver) CreateStat(ctx context.Context, stat apiModels.NewStat) (interface{}, error) {
 	mStat := &models.Stat{
 		Name: stat.Name,
 	}
-	if stat.Name != "" {
-		mStat.Name = stat.Name
+	if stat.Description != nil {
+		mStat.Description = sql.NullString{
+			String: *stat.Description,
+			Valid:  true,
+		}
 	}
+	if stat.SeqPeriod != nil {
+		mStat.SeqPeriod = sql.NullString{
+			String: *stat.SeqPeriod,
+			Valid:  true,
+		}
+	}
+	mStat.Period = *stat.Period
 
 	err := r.statService.Create(ctx, mStat)
 	if err != nil {
